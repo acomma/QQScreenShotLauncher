c4b3e5bcde71768ac51d553c6364917ff423c016
acomma <acomma@yeah.net>
2025-03-30 16:14:49 +0800

    配置设置对话框样式

--- NTLauncher/Form2.Designer.cs
+++ NTLauncher/Form2.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            SuspendLayout();
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            Text = "设置";
+            ResumeLayout(false);
         }
 
         #endregion
